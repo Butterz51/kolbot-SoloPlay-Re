57969e9ee2538adb0b56cb940f160fca136477af
Butterz51 <justin86@live.ca>
2024-12-01 12:36:52 -0500

    dev

--- libs/SoloPlay/Tools/Developer.js
+++ libs/SoloPlay/Tools/Developer.js
@@ -2,6 +2,7 @@
 *  @filename    Developer.js
 *  @author      theBGuy
 *  @desc        Tools/Settings for Kolbot-SoloPlay
+*  @contributor Butterz
 *
 */
 
@@ -10,19 +11,53 @@
  * - add override to GlobalAccount here to allow per profile options
  * - add name choices in similar manner, would have to experiment with max lengths allowed as a prefix
  */
+
 const Developer = {
+  // @desc - Global Account Settings
+  GlobalSettings: {
+    Account: "", // Set a value for a global accounts. (MAX Characters 12 plus Suffix Length)
+    Password: "", // Set a value for a global password for account generation.
+    Enable: false, // Set to true to use the "Character Name" feature.
+    Name: "", // Set a global character name. (MAX Characters 12 plus Suffix Length)
+    // @desc - Game info to use
+    GSLabel: false, // Added the Global Settings Label before the game name.
+    GameName: "", // Game Name + the profile number will be added to the end of whatever you put here 
+    GamePass: "" // Game Password
+  },
+  // @desc - Set the number of profiles sharing the same IP in this file
+  ProfilesPerIP: {
+    StaticProfiles: 1, // Change this value as needed (Max amount 8 profiles per IP).
+    // Array of profiles with the corresponding number of IPs being used
+    ProfilesArray: [
+      //{ ProfileName: "SCL-SORC-101", IP: 2 },
+      //{ ProfileName: "SCL-SORC-102", IP: 2 }
+      // Add more profiles here as needed
+    ],
+  },
+  // @desc - set to true if you want to report to https://d2soj.com for soj sales/Dclone walks
+  webSiteReport: {
+    enabled: true,
+    name: "JBH", // Name to report on site
+  },
+  consecutiveFTJ: {
+    count: 2
+  },
   // @desc - set to true if using the PlugY mod - allows use of larger stash
   plugyMode: false,
   // @desc - log game/bot statistics to .csv files located at SoloPlay/Data/
   logPerformance: true,
   // @desc - show in game overlay (see bottom of README.md for example)
   overlay: true,
-  // @desc - show Total, InGame, and OOG (out of game) time in the D2bot# status window
+  // @desc - show Total, InGame, and OOG (out of game) time in the D2Bot# status window
   displayClockInConsole: false,
+  //@desc - show game names in the D2Bot# status window
+  displayGameName: true,
   // @desc - log currently equipped items to D2Bot# charviewer tab
   logEquipped: false,
-  // @desc - disable printing chicken info in D2Bot console
+  // @desc - disable printing chicken info in D2Bot# console
   hideChickens: true,
+  // @desc - disable printing death info in D2Bot# console
+  hideDeaths: true,
   // @desc - enable ladder runewords in single player mode ONLY WORKS IF RUNEWORDS.TXT IS INSTALLED AND D2BS PROFILE IS CONFIGURED
   //   or patch.json has been updated (see Single Player Additions in README.md)
   addLadderRW: !me.profile.toLowerCase().contains("nl"),
@@ -34,12 +69,30 @@ const Developer = {
   },
   // @desc - set to true in use with tag Bumper, Socketmule, or Imbuemule to make next character after reaching goal until account is full
   fillAccount: {
-    bumpers: false,
-    socketMules: false,
-    imbueMule: false,
+    Bumper: {
+      Logging: true, // Enable logging of the account and character names in a txt file
+      Enabled: true, // Fill account (See Count)
+      Level: 40, // Set stop level for bumpers (20 Normal, 40 Nightmare, 60 Hell)
+      Count: 10, // Number of character to fill on account (MAX 18)
+      //HidePickit: true, // Set to false to allow items to be logged in the ItemLog
+      NextAccount: true, // Create a new account once the first account reaches its maximum character count or the specified limit
+    },
+    SocketMules: {
+      Logging: true, // Enable logging of the account and character names in a txt file
+      Enabled: true, // Fill account (See Count)
+      Count: 18, // Number of character to create on account (MAX 18)
+      //HidePickit: true, // Set to false to allow items to be logged in the ItemLog
+      NextAccount: true, // Create a new account once the first account reaches its maximum character count or the specified limit
+    },
+    ImbueMules: {
+      Logging: true, // Enable logging of the account and character names in a txt file
+      Enabled: false, // Fill account (See Count)
+      Level: 30, // Set stop level for imbueMule
+      Count: 18, // Number of character to create on account (MAX 18)
+      //HidePickit: true, // Set to false to allow items to be logged in the ItemLog
+      NextAccount: false, // Create a new account once the first account reaches its maximum character count or the specified limit
+    },
   },
-  // @desc - set level for imbueMule to stop at
-  imbueStopLevel: 30,
   // @desc - stop a profile once it reaches a certain level
   stopAtLevel: {
     enabled: false,
@@ -92,4 +145,114 @@ if (Developer.developerMode.enabled && Developer.developerMode.profiles.some(pro
   //Developer.debugging.skills = true;
   Developer.debugging.showStack.enabled = true;
   Developer.debugging.showStack.profiles.push(me.profile.toLowerCase());
+}
+
+
+/**
+ * Calculates and sets the minimum game time based on profile data.
+ * If the profile is found in the profiles array, calculates the minimum game time
+ * using individual profile IP count. Otherwise, uses default settings.
+ *
+ * @returns {number} The calculated minimum game time.
+ */
+
+function calculateAndSetMinGameTime () {
+  const maxAllowedProfilesPerIP = 8; // Maximum number of allowed profiles per single IP address
+  
+  try {
+    const profilesArray = Developer.ProfilesPerIP.ProfilesArray;
+    const profileInArray = profilesArray.find(profile => profile.ProfileName.toUpperCase() === me.profile.toUpperCase());
+
+    // Initialize minGameTime based on profilesArray or default
+    let minGameTime = Infinity;
+
+    if (profileInArray) {
+      // Calculate the MinGameTime based on individual profile IP count
+      let clampedNumProfiles = Math.min(profileInArray.IP, maxAllowedProfilesPerIP);
+
+      if (profileInArray.IP > maxAllowedProfilesPerIP) {
+        throw new Error("Invalid IP value in ProfilesArray. Maximum allowed is " + maxAllowedProfilesPerIP);
+      }
+
+      minGameTime = calculateMinGameTime(clampedNumProfiles);
+    } else {
+      // Calculate the MinGameTime using default settings
+      const numProfiles = Developer.ProfilesPerIP.StaticProfiles;
+      let clampedNumProfiles = Math.min(numProfiles, maxAllowedProfilesPerIP);
+
+      if (numProfiles > maxAllowedProfilesPerIP) {
+        throw new Error("Invalid numProfiles value. Maximum allowed is " + maxAllowedProfilesPerIP);
+      }
+
+      minGameTime = calculateMinGameTime(clampedNumProfiles);
+    }
+
+    return minGameTime;
+  } catch (error) {
+     // Handle any errors by returning a default value (180 sec in this case)
+    return 180;
+  }
+}
+
+/**
+ * Calculates the minimum game time based on the number of profiles,
+ * taking into account the maximum allowed profiles per IP address.
+ *
+ * @param {number} numProfiles - The number of profiles sharing the same IP address.
+ * @returns {number} The calculated minimum game time.
+ */
+
+function calculateMinGameTime (numProfiles) {
+  // Maximum number of allowed profiles per single IP address
+  const maxAllowedProfilesPerIP = 8;
+
+  // Default Game Time Configuration
+  const defaultMinGameTime = 600; // Default game duration (in seconds) for a single profile on one IP address (Super Safe 11 minutes)
+
+  // Calculate additional game time based on the number of profiles
+  const additionalMinGameTimePerProfile = numProfiles >= 2 && numProfiles < 3 ? 300 : 0; // Adds 5 minutes (300 seconds) for each extra profile (Max 2 Profiles)
+  const additionalProfileTimeSmallGroup = numProfiles >= 3 && numProfiles < 4 ? 900 : 0; // Adds 15 minutes (900 seconds) for smaller groups (Max 3 Profiles)
+  const additionalProfileTimeLargeGroup = numProfiles >= 4 && numProfiles < 8 ? 1500 : 0; // Adds 25 minutes (1500 seconds) for larger groups (Max 4-8 Profiles)
+
+  // Calculate the MinGameTime using the configured settings
+  const clampedNumProfiles = Math.min(numProfiles, maxAllowedProfilesPerIP);
+  const additionalTime = (clampedNumProfiles - 1) * additionalMinGameTimePerProfile + additionalProfileTimeSmallGroup + additionalProfileTimeLargeGroup;
+
+  // Calculate the final minimum game time
+  const minGameTime = defaultMinGameTime + additionalTime;
+
+  return minGameTime;
+}
+
+/**
+ * Validates the profile IP and numProfiles values, printing errors if necessary.
+ *
+ * @param {object} profileInArray - The profile information from profiles array.
+ * @param {number} numProfiles - The number of profiles sharing the same IP address.
+ */
+
+function validateProfileIP(numProfiles) {
+  const maxAllowedValue = 8; // Maximum number of allowed profiles per single IP address
+  const minNotAllowed = 0; // Minimum allowed value for IP and numProfiles
+  const profilesArray = Developer.ProfilesPerIP.ProfilesArray;
+  const profileInArray = profilesArray.find(profile => profile.ProfileName.toUpperCase() === me.profile.toUpperCase());
+
+  let errorMsg = "";
+
+  if (numProfiles > maxAllowedValue || numProfiles < minNotAllowed) {
+    errorMsg = 
+      (
+        profileInArray
+          ? "The value exceeds the allowable limits. For the profile " + profileInArray.ProfileName + ", which has an associated IP value of " + profileInArray.IP
+          : "The value exceeds the allowable static limits."
+      )
+      + (numProfiles > maxAllowedValue 
+          ? " The maximum allowable limit is " + maxAllowedValue + ". Using the default value of 3 minutes."
+          : " The minimum allowable limit is 1. Using the default value of 3 minutes."
+      );
+  }
+
+  if (errorMsg) {
+    D2Bot.printToConsole(errorMsg, sdk.colors.D2Bot.Red);
+  }
 }
